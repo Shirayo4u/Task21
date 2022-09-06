2f64945ef66b0b38edf6d5f69db4e092429ebcf5
remix1or <remixtor@gmail.com>
2022-09-06 22:00:01 +0300

    init commit2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -36,5 +36,6 @@ public class Util {
                 .applySettings(configuration.getProperties()).build();
         return configuration.buildSessionFactory(serviceRegistry);
     }
+    //sdfgjkjkgsjdfjkskd
 }
 
